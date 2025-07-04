33cb3e01f40e36f25a0d61ecbde87a8ce6c164eb
eaysin <eaysin.dev@gmail.com>
2025-07-05 05:06:47 +0600

    feat: add dynamic server configuration for Swagger UI
    Updated Swagger API configuration to include dynamic server URLs based on environment (production or development). Enhanced flexibility for OpenAPI documentation across deployments.

--- src/routes/swagger.route.ts
+++ src/routes/swagger.route.ts
@@ -6,22 +6,25 @@ import {appConfig} from "../config/default-config/app-config";
 
 const router = express.Router();
 
-// Load your OpenAPI YAML file
 const openapiPath = path.join(__dirname, '../../openapi.yaml');
 const openapiSpec = YAML.load(openapiPath);
 
-// Merge with your existing swagger definition
+const servers = [
+    {
+        url: process.env.NODE_ENV === 'production'
+            ? 'https://marketing-server-eight.vercel.app/api/v1'
+            : `http://localhost:${appConfig.port}/api/v1`,
+        description: process.env.NODE_ENV === 'production'
+            ? 'Production Server'
+            : 'Development Server',
+    },
+];
+
 const mergedSpec = {
     ...openapiSpec,
-    servers: [
-        {
-            url: `http://localhost:${appConfig.port}/api/v1`,
-            description: 'Development Server',
-        },
-    ],
+    servers
 };
 
-// Serve Swagger UI
 router.use('/', swaggerUi.serve);
 router.get('/', swaggerUi.setup(mergedSpec, {
     explorer: true,
